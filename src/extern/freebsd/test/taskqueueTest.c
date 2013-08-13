5f977d882e2b6b80371d6f537456b357c81013e4
Sukwon Oh <sukwon0709@gmail.com>
2013-08-13 13:35:00 -0400

    Compiled subr_bus.c

--- src/extern/freebsd/test/taskqueueTest.c
+++ src/extern/freebsd/test/taskqueueTest.c
@@ -9,6 +9,10 @@ void func1(void *context, int pending) {
   printf("func1 ran!\n");
 }
 
+void func2(void *context, int pending) {
+  printf("func2 ran!\n");
+}
+
 void taskqueueTest1() {
   printf("\nrunning taskqueueTest1...\n");
   struct task *t = malloc(sizeof(struct task), 0, M_WAITOK);
@@ -19,4 +23,15 @@ void taskqueueTest1() {
   free(t, 0);
 }
 
+void taskqueueTest2() {
+  printf("\nrunning taskqueueTest2...\n");
+  struct task *t = malloc(sizeof(struct task), 0, M_WAITOK);
+  TASK_INIT(t, 0, func2, NULL);
+  taskqueue_enqueue(taskqueue_swi, t);
+  taskqueue_drain(taskqueue_swi, t);
+  printf("taskqueueTest2 done!\n");
+  free(t, 0);
+}
+
 SYSINIT(taskqueueTest1, SI_SUB_RUN_SCHEDULER, SI_ORDER_FIRST, taskqueueTest1, NULL);
+SYSINIT(taskqueueTest2, SI_SUB_RUN_SCHEDULER, SI_ORDER_FIRST, taskqueueTest2, NULL);
